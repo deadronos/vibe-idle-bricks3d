e21883062ff57c39deaaea9547b1239a17f75c5e
deadronos <iridanblub@hotmail.de>
2025-12-01 07:23:28 +0100

    feat: update camera position and controls for improved user experience

--- src/components/GameScene.tsx
+++ src/components/GameScene.tsx
@@ -26,14 +26,18 @@ function GameContent() {
 
   return (
     <>
-      {/* Camera */}
-      <PerspectiveCamera makeDefault position={[0, 0, 20]} fov={50} />
+      {/* Camera: start angled and slowly auto-rotate like the screenshot */}
+      <PerspectiveCamera makeDefault position={[10, 6, 10]} fov={50} />
       <OrbitControls
         enablePan={false}
         minDistance={10}
         maxDistance={40}
         minPolarAngle={Math.PI / 6}
         maxPolarAngle={Math.PI / 2}
+        enableDamping
+        dampingFactor={0.08}
+        autoRotate
+        autoRotateSpeed={0.6}
       />
 
       {/* Lighting */}
